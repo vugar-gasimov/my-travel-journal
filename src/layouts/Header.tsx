c938dbb906ce02979c1b78111356d641459fce03
vugar-gasimov <vuqar5qasimov@gmail.com>
2023-12-30 03:50:53 +0200

    Responsivnes in header.

--- src/layouts/Header.tsx
+++ src/layouts/Header.tsx
@@ -1,9 +1,9 @@
 import { FaGlobeEurope } from "react-icons/fa";
 export function Header() {
   return (
-    <div className=" flex justify-center items-center w-full h-[55px]  bg-[#f55959] gap-1">
-      <FaGlobeEurope color="white" size={24} />
-      <h1 className=" h-4 text-white text-sm font-medium font-Inter">
+    <div className=" flex justify-center items-center w-full lg:h-[110px] h-[55px]  bg-[#f55959] lg:gap-2 gap-1">
+      <FaGlobeEurope className="text-white text-3xl text-[24px] lg:text-[36px]" />
+      <h1 className=" text-white lg:text-[30px] text-[15px] font-medium font-Inter">
         My travel journal.
       </h1>
     </div>
