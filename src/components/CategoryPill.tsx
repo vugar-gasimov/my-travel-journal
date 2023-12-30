ddd042e41c71b5195ba7ab4bc037eb27da840935
vugar-gasimov <vuqar5qasimov@gmail.com>
2023-12-30 03:33:50 +0200

    Readme and larg screen design.

--- src/components/CategoryPill.tsx
+++ src/components/CategoryPill.tsx
@@ -14,34 +14,34 @@ export function CategoryPill(pill: CategoryPillProps) {
   const { country, info, title, description, time, coverImg } = pill;
   return (
     <section className="flex justify-center flex-col  items-center">
-      <div className="flex justify-center flex-row gap-5 w-478 mb-2 mt-2 bg-cover">
+      <div className="flex justify-center  flex-row  gap-5 lg:w-full w-478 lg:mb-4 lg:mt-4 mb-2 mt-2 bg-cover">
         <img
-          className="w-[125px] h-[168px] rounded-[5px] object-cover"
+          className="lg:w-[270px] lg:h-[360px] w-[125px] h-[168px] rounded-[5px] object-cover"
           src={coverImg}
           alt="Countries image"
         />
         <div className="flex justify-start flex-col ">
-          <p className="flex gap-1 h-2.5 text-gray-800 text-[10.24px] font-normal font-['Inter'] tracking-widest mb-2">
+          <p className="flex gap-1 h-2.5 text-gray-800 lg:text-[21px] text-[10.24px] font-normal font-['Inter'] tracking-widest lg:mb-4 mb-2">
             <FaLocationDot color="red" /> {country}
             <a
               href="example.com"
-              className=" h-[13px] text-neutral-400 text-[10.24px] font-normal font-['Inter'] underline"
+              className=" h-[13px] text-neutral-400 lg:text-[21px] text-[10.24px] font-normal font-['Inter'] underline"
             >
               {info}
             </a>
           </p>
-          <h1 className="w-[326px] h-[33px] text-gray-800 text-[25px] font-bold font-['Inter'] mb-4">
+          <h1 className="lg:w-[436px] w-[326px]  text-gray-800 lg:text-[45px] text-[25px] font-bold font-['Inter'] lg:mb-8 mb-4  lg:tracking-[-2px]">
             {title}
           </h1>
-          <h2 className="w-[319px] h-[11px] text-gray-800 text-[10.24px] font-bold font-['Inter'] mb-2">
+          <h2 className="w-[319px]  text-gray-800 lg:text-[21px] text-[10.24px] font-bold font-['Inter'] lg:mb-4 mb-2">
             {time}
           </h2>
-          <p className="w-[326px] text-gray-800 text-[10.24px] font-normal font-['Inter'] leading-none">
+          <p className=" lg:w-[436px] w-[326px] text-gray-800 lg:text-[21px] text-[10.24px] font-normal font-['Inter'] leading-none">
             {description}
           </p>
         </div>
       </div>
-      <div className="w-[478px] h-[0px] border border-neutral-100"></div>
+      <div className=" lg:w-[900px] w-[478px] h-[0px] border border-neutral-100"></div>
     </section>
   );
 }
